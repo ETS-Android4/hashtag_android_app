af806c71490b3c45a72513472c4776ecbe125c46
Bibash09 <bib6466@gmail.com>
2022-04-19 11:39:49 +0545

    second commit

--- app/src/main/java/com/ecommerce/hashtagcollection/Login_Activity.java
+++ app/src/main/java/com/ecommerce/hashtagcollection/Login_Activity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login_Activity extends AppCompatActivity {
 
@@ -21,7 +22,18 @@ public class Login_Activity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.etPasswordLogin);
         signIn = (EditText) findViewById(R.id.etSignIn);
         login = (Button) findViewById(R.id.btnLogin);
-    }
 
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (email.getText().toString().equals("admin@gmail.com") && password.getText().toString().equals("Root123"))
+                    //correct
+                    Toast.makeText(MainActivity.this,"Login sucessfully",Toast.LENGTH_LONG.show());
+            }
+        });
+
+
+    }
 
 }
